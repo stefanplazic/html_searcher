5d72ec24eba4205d825c996c996055f147c76d4b
stefanplazic <stefan.plazic@gmail.com>
2017-06-20 17:35:48 +0200

    Bag of words created

--- search.py
+++ search.py
@@ -21,6 +21,36 @@ def get_all_Files( dir_Path='python-2.7.7-docs-html'):
 	return my_files
 
 
+def create_dictionary(html_files = []):
+	
+	#dic to holed data for all pages
+	my_pages = {}
+	
+	#create parser
+	parser = Parser()
+
+	for file in html_files:
+		[links, words] = parser.parse(file)
+		my_pages[file] = words_frequency(words)
+
+	return my_pages
+
+
+def words_frequency(list_of_words):
+	
+	bags_of_words = {}
+
+	for word in list_of_words:
+		
+		if word in bags_of_words:
+			#if word is already in the list - increment
+			bags_of_words[word] += 1
+
+		else:
+			bags_of_words[word] = 1
+
+	return bags_of_words
+	
 
 if __name__=='__main__':
 	
@@ -28,9 +58,12 @@ if __name__=='__main__':
 	file_path = sys.argv[1]
 
 	html_files = get_all_Files(file_path)
+
+	my_pages = create_dictionary(html_files)
+	print my_pages
 	
-	parser = Parser()
-	#[links, words] = parser.parse(file_path)
+	
+	
 
 	
 	
